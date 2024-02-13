5f0d7f4ae5b624b2ed9dba4dbfa16c3990b69b3b
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-02-12 19:45:31 -0500

    First commit

--- src/lib/fragment.glsl
+++ src/lib/fragment.glsl
@@ -3,8 +3,20 @@
 uniform float uTime;
 varying vec2 vUv;
 
+float random(vec2 st){
+    // Create a unique float value based on st
+    return fract(sin(dot(st.xy,vec2(12.9898,78.233)))*43758.5453123);
+}
+
 void main(void)
 {
-    vec4 color=vec4(vUv,1.,1.);
+    vec2 uv=vUv;
+    
+    vec4 color1=vec4(.9529,.6353,0.,1.);
+    vec4 color2=vec4(.9,.8,1.,1.);
+    
+    float f=smoothstep(0.,2.,uv.x+random(uv));
+    
+    vec4 color=mix(color1,color2,f);
     gl_FragColor=color;
 }
