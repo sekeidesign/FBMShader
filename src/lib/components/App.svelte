f288f2e122f2cbb1f6d9e5fe050dbc341d8cfff7
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-02-17 11:42:48 -0500

    change responsive breakpoint

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -71,7 +71,7 @@
 	<div class="absolute z-10 w-full h-full bg-white loader-white"></div>
 </div>
 <div
-	class="grid grid-rows-8 grid-cols-4 md:grid-cols-10 p-10 z-10 fixed h-screen w-screen font-aeonik text-white"
+	class="grid grid-rows-8 grid-cols-4 lg:grid-cols-10 p-10 z-10 fixed h-screen w-screen font-aeonik text-white"
 >
 	<div class="flex gap-1.5 items-start col-span-4 row-start-6 logo">
 		<img
@@ -86,8 +86,8 @@
 			style="height: clamp(.75rem, 3vh, 1.75rem); opacity: 0; transform: translateY(32px);"
 		/>
 	</div>
-	<p class="text-xl col-span-3 md:col-start-6 row-start-7 md:row-start-6 text-1"></p>
-	<p class="text-xl col-span-3 md:col-start-6 row-start-8 md:row-start-7 text-2"></p>
+	<p class="text-xl col-span-3 lg:col-start-6 row-start-7 lg:row-start-6 text-1"></p>
+	<p class="text-xl col-span-3 lg:col-start-6 row-start-8 lg:row-start-7 text-2"></p>
 </div>
 <div class="canvas-size">
 	<Canvas>
